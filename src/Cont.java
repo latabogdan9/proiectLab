fa1e9b426321748181e42c5a1ded349c5024a299
latabogdan9 <z.z59040@yahoo.com>
2021-10-05 00:55:02 +0300

    Bank app - redesigned menu for login option -original menu now after you login -created account class and instance

---
+++ src/Cont.java
@@ -0,0 +1,16 @@
+public class Cont {
+    String nume;
+    String prenume;
+    String cnp;
+    String bank;
+    int id;
+    public Cont(String bank, String nume, String prenume, String cnp, int id) {
+        this.bank = bank;
+        this.nume = nume;
+        this.prenume = prenume;
+        this.cnp = cnp;
+        this.id = id;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,59 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(" /$$$$$$$");
+        System.out.println("| $$__  $$ ");
+        System.out.println("| $$  \\ $$  /$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$ ");
+        System.out.println("| $$$$$$$  |____  $$| $$__  $$ /$$_____/ |____  $$");
+        System.out.println("| $$__  $$  /$$$$$$$| $$  \\ $$| $$        /$$$$$$$");
+        System.out.println("| $$  \\ $$ /$$__  $$| $$  | $$| $$       /$$__  $$");
+        System.out.println("| $$$$$$$/|  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$$");
+        System.out.println("|_______/  \\_______/|__/  |__/ \\_______/ \\_______/");
         int op;
         int stop=0;
+        int id = 0;
+        String bank, nume, prenume, cnp;
+        Cont c[] = new Cont[10];
         while (stop==0){
+
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Alegeti o optiune:");
+            System.out.println("1.Creati cont");
+            System.out.println("2.Intrare in cont");
+
+            System.out.println("0.Inchideti programul");
+
+            op = scan.nextInt();
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+            if (op==0){
+                stop = 1;
+            }
+            if(op == 1) {
+                id++;
+                System.out.println("Introduceti banca la care doriti sa va faceti cont: ");
+                bank = scan.next();
+                System.out.println("Introduceti numele dumneavoasra: ");
+                nume = scan.next();
+                System.out.println("Introduceti prenumele dumneavoasra: ");
+                prenume = scan.next();
+                System.out.println("Introduceti CNP-ul dumneavoasra: ");
+                cnp = scan.next();
+
+                c[id] = new Cont(bank,nume, prenume, cnp, id);
+            }
+            if(op == 2){
+                System.out.println("Introduceti CNP-ul dumneavoastra: ");
+
+            }
+        }
+    }
+}
+
+
+/*
+System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
             Scanner scan = new Scanner(System.in);
             System.out.println("Alegeti o optiune:");
@@ -17,11 +66,5 @@ public class Main {
             System.out.println("0.Inchideti programul");
 
             op = scan.nextInt();
-
-            if (op==0){
-                stop = 1;
-            }
-
-        }
-    }
-}
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+ */
